2ce10814f2253e137057e84fc0299c9a0e3b6b7a
oppong <michaeloppong123@gmail.com>
2023-03-05 15:39:18 -0500

    chat works perfectly,

--- screens/ChatScreen2.jsx
+++ screens/ChatScreen2.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
 import ChatHeader from "../components/ChatHeader";
@@ -8,27 +9,72 @@ import { renderComposer } from "../components/renderComposer";
 import { renderInputToolbar } from "../components/renderInputToolBar";
 import { renderSend } from "../components/renderSend";
 import { theme } from "../styles/theme";
-import { fakeMessages } from "../util/constants";
+const API = "http://localhost:5000";
 const ChatScreen2 = () => {
-  const [messages, setMessages] = useState([]);
+  const [allMessages, setAllMessages] = useState([]);
   const [text, setText] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setMessages(fakeMessages);
-  }, []);
+  const createPrompt = (messages) => {
+    let prompt = "";
 
-  const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages) =>
+    messages.reverse().forEach((message) => {
+      if (message.user._id === 1) {
+        prompt += `You: ${message.text}\nBot: `;
+      } else {
+        prompt += `${message.text}\n`;
+      }
+    });
+    return prompt;
+  };
+
+  const onSend = async (messages = []) => {
+    const combinedMessages = GiftedChat.append(allMessages, messages);
+    setAllMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages)
     );
-  }, []);
+    setLoading(true);
+
+    combinedMessages.forEach((message, index) => {
+      console.log(index, message.text);
+    });
+    const prompt = createPrompt(combinedMessages);
+
+    try {
+      const res = await axios.post(`${API}/api/openai`, {
+        prompt: prompt,
+      });
+      console.log("response", res.data.data);
+
+      setAllMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, [
+          {
+            _id: Math.random(),
+            text: res.data.data,
+            createdAt: new Date(),
+            image: "https://placeimg.com/140/140/any",
+            user: {
+              _id: 2,
+              avatar: "https://placeimg.com/140/140/any",
+            },
+          },
+        ])
+      );
+    } catch (error) {
+      console.log("full");
+    }
+
+    setLoading(false);
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: theme.colors.black }}>
       <ChatHeader />
       <GiftedChat
+        renderMessageImage={() => null}
         renderTime={() => null}
-        messages={messages}
+        messages={allMessages}
+        isTyping={loading}
         placeholder="Message..."
         renderComposer={renderComposer}
         messagesContainerStyle={{
--- util/constants.js
+++ util/constants.js
@@ -1,31 +1,11 @@
 export const fakeMessages = [
   {
     _id: 1,
-    text: "Follow @sobored.ca on ig",
+    text: "Hey there!",
     createdAt: new Date(),
     user: {
       _id: 2,
-      name: "React Native",
-      avatar: "https://placeimg.com/140/140/any",
-    },
-  },
-  {
-    _id: 44353,
-    text: "Follow @sobored.ca on ig okayy",
-    createdAt: new Date(),
-    user: {
-      _id: 2,
-      name: "React Native",
-      avatar: "https://placeimg.com/140/140/any",
-    },
-  },
-  {
-    _id: 4433353,
-    text: "Follow @sobored.ca on ig okayy",
-    createdAt: new Date(),
-    user: {
-      _id: 2,
-      name: "React Native",
+      name: "Bot",
       avatar: "https://placeimg.com/140/140/any",
     },
   },
